c85fb664237701cabd0d563a80c157fe65c20568
Trường Nguyễn <nguyenvantruong9902@gmail.com>
2020-01-03 11:25:29 +0700

    Stable

--- BookStoreAPI/App_Start/WebApiConfig.cs
+++ BookStoreAPI/App_Start/WebApiConfig.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Formatting;
 using System.Web.Http;
 
 namespace BookStoreAPI
@@ -19,6 +20,8 @@ namespace BookStoreAPI
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+            config.Formatters.Clear();
+            config.Formatters.Add(new JsonMediaTypeFormatter());
         }
     }
 }
--- DataModel/BookStoreContext.cs
+++ DataModel/BookStoreContext.cs
@@ -6,26 +6,9 @@ namespace DataModel
 
     public class BookStoreContext : DbContext
     {
-        // Your context has been configured to use a 'BookStoreContext' connection string from your application's 
-        // configuration file (App.config or Web.config). By default, this connection string targets the 
-        // 'DataModel.BookStoreContext' database on your LocalDb instance. 
-        // 
-        // If you wish to target a different database and/or database provider, modify the 'BookStoreContext' 
-        // connection string in the application configuration file.
         public BookStoreContext()
             : base("name=BookStoreContext")
         {
         }
-
-        // Add a DbSet for each entity type that you want to include in your model. For more information 
-        // on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
-
-        // public virtual DbSet<MyEntity> MyEntities { get; set; }
     }
-
-    //public class MyEntity
-    //{
-    //    public int Id { get; set; }
-    //    public string Name { get; set; }
-    //}
 }
